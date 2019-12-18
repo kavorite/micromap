90d7383fd2b31799991f49549e7540aeb451965b
kavorite <iseurie@gmail.com>
2019-12-18 14:59:52 -0500

    test reallocation

--- test.c
+++ test.c
@@ -5,28 +5,33 @@
 #include <stdio.h>
 #include "micromap.h"
 
-int main(int argc, char** argv) {
+int main(void) {
     ledger dict = { .cells = NULL, .len = 0, .cap = 0};
     tbGrow(&dict, 32);
     srand(time(0));
     char keys[22][9];
     unsigned int vals[22];
+    tbStatus stat;
     for (int i = 0; i < 22; i++) {
         unsigned int x = rand();
         // 64 bits = 8 bytes + null terminator
         vals[i] = x;
         sprintf(keys[i], "%08x", x);
-        tbStatus stat;
         if((stat = tbSet(&dict, keys[i], vals+i)) != TB_STAT_OK) {
             fprintf(stderr, "fatal: failed ledger insertion: %s\n", tbStrError(stat));
             return -1;
         }
     }
+    printf("load factor pre-growth: %f\n", (float)(dict.len) / (float)(dict.cap));
     // try doing a brain transplant
-    tbGrow(&dict, 64);
+    if ((stat = tbGrow(&dict, 64)) != TB_STAT_OK) {
+        fprintf(stderr, "fatal: failed ledger rehash/reallocation: %s\n", tbStrError(stat));
+        return -2;
+    }
+    printf("load factor post-growth: %f\n", (float)(dict.len) / (float)(dict.cap));
     if (dict.len != 22) {
         fprintf(stderr, "fatal: length does not match\n");
-        return -2;
+        return -3;
     }
     // print key/value pairs
     for (int i = 0; i < dict.len; i++) {
@@ -38,4 +43,6 @@ int main(int argc, char** argv) {
         }
     }
     tbFree(&dict);
+    printf("OK\n");
+    return 0;
 }
